9af95dee4d3b18189fc36128a3e5607d9ce359f5
Kebechet <samosidor@gmail.com>
2025-11-07 07:13:23 +0100

    added event handler mappers

---
+++ src/Blazor.EventHandlers.LongPress/Extensions/LongPressEventArgsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace Blazor.EventHandlers.LongPress.Extensions;
+
+public static class LongPressEventArgsExtensions
+{
+    public static MouseEventArgs ToMouseEventArgs(this LongPressEventArgs longPressEventArgs)
+    {
+        return new MouseEventArgs
+        {
+            ScreenX = longPressEventArgs.ScreenX,
+            ScreenY = longPressEventArgs.ScreenY,
+            ClientX = longPressEventArgs.ClientX,
+            ClientY = longPressEventArgs.ClientY,
+            OffsetX = longPressEventArgs.OffsetX,
+            OffsetY = longPressEventArgs.OffsetY,
+            PageX = longPressEventArgs.PageX,
+            PageY = longPressEventArgs.PageY,
+            Type = longPressEventArgs.Type ?? string.Empty,
+        };
+    }
+}
---
+++ src/Blazor.EventHandlers.LongPress/Extensions/MouseEventArgsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace Blazor.EventHandlers.LongPress.Extensions;
+
+public static class MouseEventArgsExtensions
+{
+    public static LongPressEventArgs ToLongPressEventArgs(this MouseEventArgs mouseEventArgs)
+    {
+        return new LongPressEventArgs
+        {
+            ScreenX = mouseEventArgs.ScreenX,
+            ScreenY = mouseEventArgs.ScreenY,
+            ClientX = mouseEventArgs.ClientX,
+            ClientY = mouseEventArgs.ClientY,
+            OffsetX = mouseEventArgs.OffsetX,
+            OffsetY = mouseEventArgs.OffsetY,
+            PageX = mouseEventArgs.PageX,
+            PageY = mouseEventArgs.PageY,
+            Type = mouseEventArgs.Type
+        };
+    }
+}
